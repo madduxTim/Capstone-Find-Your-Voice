c584491d1c431761772bae4fe5928607f420dd5f
madduxTim <timothymaddux@gmail.com>
2016-06-14 08:32:05 -0500

    Commit in case OS updates

--- app/controllers/LandingSearchCTRL.js
+++ app/controllers/LandingSearchCTRL.js
@@ -1,7 +1,7 @@
 "use strict";
 // FILE PAIRS WITH MAIN.HTML AND SEARCH-FACTORY.JS
 
-app.controller("LandingSearchCTRL", function($scope, searchFactory) { // add in keywordCallStorage
+app.controller("LandingSearchCTRL", function($scope, $location, searchFactory) { // add in keywordCallStorage
     $scope.queryStorage = [];
 
     $scope.searchCall = () => {
@@ -10,5 +10,15 @@ app.controller("LandingSearchCTRL", function($scope, searchFactory) { // add in 
                 console.log(results);
                 $scope.queryStorage = results;
         });
+    };
+
+    $scope.hrefFix = (billId) => {
+        // Need to perform a foreach on billId to get the output right
+        // AND THEN re-route the user to right place on 
+        // http://www.capitol.tn.gov/Bills/109/Bill/SB0324.pdf  (zum bispiel)
+        Materialize.toast(billId, 3000);
+        location.replace(`http://www.capitol.tn.gov/Bills/109/Bill/${billId}.pdf`);
+        // Materialize.toast(billId.length, 3000, "blue");
+        
     };
 });
