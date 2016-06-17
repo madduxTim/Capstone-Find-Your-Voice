ea2289c20a5badcd9ca7d1f1c527f8b7995581ab
madduxTim <timothymaddux@gmail.com>
2016-06-17 11:20:16 -0500

    Authentication essentials added

--- app/app.js
+++ app/app.js
@@ -2,19 +2,44 @@
 var app = angular.module("FindYourVoice", ["ngRoute"])
     .constant("firebaseURL", "https://find-your-voice.firebaseio.com/");
 
+let isAuth = (AuthFactory) => new Promise ((resolve, reject) => {
+  if(AuthFactory.isAuthenticated()){
+    console.log("User is authenticated, resolve route promise");
+    resolve();
+  } else {
+    console.log("User is not authenticated, reject route promise");
+    reject();
+  }
+});
+
 app.config(function($routeProvider){
     $routeProvider.
         when("/", {
             templateUrl: "partials/main.html",
-            controller: "SearchCTRL"
+            controller: "SearchCTRL",
+            resolve: {isAuth}
         }).
         when("/main", {
             templateUrl: "partials/main.html",
-            controller: "SearchCTRL"
+            controller: "SearchCTRL",
+            resolve: {isAuth}
         }).
         when("/login", {
             templateUrl: "partials/login.html",
             controller: "AuthCTRL"
         }).
+        when("/logout", {
+            templateUrl: "partials/login.html",
+            controller: "AuthCTRL"
+        }).
         otherwise("/");
+});
+
+app.run(($location) => {
+    let contactsRef = new Firebase("https://contacts-app-tdm.firebaseio.com/");
+    contactsRef.onAuth(authData => {
+        if(!authData){
+            $location.path("/login");
+        }
+    });
 });
--- app/controllers/AuthCTRL.js
+++ app/controllers/AuthCTRL.js
@@ -1,5 +1,39 @@
 "use strict";
 
-app.controller("AuthCTRL", function(){
-    Materialize.toast("AuthCTRL working", 1000, "rounded");
+app.controller("AuthCTRL", function($scope, $rootScope, $location, firebaseURL, AuthFactory){
+    let ref = new Firebase(firebaseURL);
+    $scope.account = {
+        email: "",
+        password: ""
+    };
+
+    if($location.path() === "/logout"){
+        ref.unauth();
+        $rootScope.isActive = false;
+    }
+
+    $scope.register = () => {
+        ref.createUser({
+            email: $scope.account.email,
+            password: $scope.account.password
+        }, (error, userData) => {
+            if(error){
+                console.log(`Error creating user: ${error}`);
+            } else{
+                console.log(`Created user account with uid: ${userData.uid}`);
+                $scope.login();
+            }
+        });
+    };
+
+
+    $scope.login = () => {
+        AuthFactory
+            .authenticate($scope.account)
+            .then(() => {
+                $rootScope.isActive = true;
+                $location.path("/");
+                $scope.$apply();
+            });
+    };
 });
