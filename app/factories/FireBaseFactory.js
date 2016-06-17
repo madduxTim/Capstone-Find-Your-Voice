a39e684d5f684092ec65ce2b5851eda474f17095
madduxTim <timothymaddux@gmail.com>
2016-06-17 12:03:50 -0500

    Fixed minor userID bug

--- app/factories/FireBaseFactory.js
+++ app/factories/FireBaseFactory.js
@@ -1,12 +1,19 @@
-app.factory("fireBaseFactory", function($q, $http, firebaseURL){
+app.factory("fireBaseFactory", function($q, $http, firebaseURL, AuthFactory){
 
     var postBill = (bill) => {
+        let user = AuthFactory.getUser();
         return $q(function(resolve, reject){
             $http
                 .post(firebaseURL + "saved-bills.json",
                     JSON.stringify({
-                        title: bill.title
-                        // bill_id: bill_id
+                        uid: user.uid,
+                        title: bill.title,
+                        billNumber: bill.bill_id,
+                        openStatesID: bill.id,
+                        session: bill.session,
+                        sponsors: bill.sponsors[0].name,
+                        billPage: bill.sources[0].url,
+                        billText: bill.versions[0].url
                     }))
                 .success(function(firebaseObj) {
                     resolve(firebaseObj);
